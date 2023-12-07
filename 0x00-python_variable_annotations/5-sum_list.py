aa66e35a95bee37503e05e9f30a10454ab7adcd7
Mohammednur <mamebb2023@gmail.com>
2023-12-07 20:54:23 +0300

    Sum List

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Python file """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ Sums up the floats in the list """
     sum = 0
     for i in input_list:

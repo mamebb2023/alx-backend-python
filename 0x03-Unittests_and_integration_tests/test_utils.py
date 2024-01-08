9d0070fa5a9d8000664e77c16fa30d5dcacb9f86
Mohammednur <mamebb2023@gmail.com>
2024-01-08 19:07:03 +0300

    Test Access Nested Map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
-""" Test utils
 """
+Test utils.py
+"""
+
 import unittest
 from typing import Callable, Dict, Mapping, Sequence
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
+
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Test Class access_nested_map
+    """ Tests access_nested_map function
     """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_app(self, nested_map: Mapping, path: Sequence, expected: int) -> None:
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: int) -> None:
+        """ Tests access_nested_map function
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
